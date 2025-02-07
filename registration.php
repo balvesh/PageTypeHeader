ff85c664ebbe0e9cacddde7030c8608ec1bf0aef
Balvesh Rakkar <85616725+balvesh@users.noreply.github.com>
2025-02-07 10:31:07 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Vendor_PageTypeHeader',
+    'Balveshrakkar_PageTypeHeader',
     __DIR__
 );
